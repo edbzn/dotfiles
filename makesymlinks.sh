bfa70dd0d2b4723e1dd3cabe971d668bcffa91c0
edouardbozon <bozon.edouard@live.fr>
2016-04-28 09:05:32 +0200

    Add git

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="atom oh-my-zsh"    # list of files/folders to symlink in homedir
+files="atom oh-my-zsh gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
