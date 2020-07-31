40c5c1b48eae2485fcd6d75761ec193903a06af6
Edouard Bozon <bozonedouard@gmail.com>
2020-07-31 23:36:39 +0200

    Fix yarn install

--- install/yarn.sh
+++ install/yarn.sh
@@ -6,4 +6,5 @@ git clone https://github.com/buonomo/yarn-completion ~/.oh-my-zsh/custom/plugins
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+sudo apt update
 sudo apt install yarn -y
