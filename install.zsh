3a6d7e48da473aa3c4692e968b5462c218747f21
Edouard Bozon <bozonedouard@gmail.com>
2021-03-17 21:41:29 +0100

    Update dir structure

---
+++ install.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# Ensure dotfiles were installed in home directory.
+dir=~/.dotfiles
+if [ ! -d "$dir" ]; then
+  echo -e  "❌ Error: Directory $dir not found."
+  exit 1
+fi
+
+# Create work folder.
+mkdir -p ~/work
+
+# Run the install scripts.
+for script in (.scripts/*); chmod +x $script && sh $script
+
+# Configure dotfiles.
+for dotfile in *(/); stow -v -t ~/ -S $dotfile
