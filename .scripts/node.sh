f0b58318558908cc7e05e16827355f82c3305a13
Edouard Bozon <bozonedouard@gmail.com>
2021-03-17 21:49:22 +0100

    Split scripts

---
+++ .scripts/node.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt install nodejs npm -y
+sudo npm install npm n -g
+
+# Take ownership to allow n without sudo
+sudo mkdir -p /usr/local/n
+sudo chown -R $(whoami) /usr/local/n
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
+# Switch to the last LTS Node.js version
+n lts
