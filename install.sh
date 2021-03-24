6ce8c4296f6177200449e0a5326451d056437683
Edouard Bozon <bozonedouard@gmail.com>
2021-03-24 20:42:00 +0100

    Setup Ansible + node playbook

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+set -e 
 
-# Ensure dotfiles were installed in home directory.
-dir=~/.dotfiles
-if [ ! -d "$dir" ]; then
-  echo -e  "❌ Directory $dir not found, .dotfiles should be cloned in home directory."
-  exit 1
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo apt install ansible
 fi
 
-# Create work folder.
-mkdir -p ~/work
 
-# Run the install scripts.
-for script in .scripts/*; do
-  chmod +x $script && sh $script
-done
+# # Ensure dotfiles were installed in home directory.
+# dir=~/.dotfiles
+# if [ ! -d "$dir" ]; then
+#   echo -e  "❌ Directory $dir not found, .dotfiles should be cloned in home directory."
+#   exit 1
+# fi
 
-# Configure dotfiles.
-for dotfile in *(/); do
-  stow -v -t ~/ -S $dotfile
-done
+# # Create work folder.
+# mkdir -p ~/work
+
+# # Run the install scripts.
+# for script in .scripts/*; do
+#   chmod +x $script && sh $script
+# done
+
+# # Configure dotfiles.
+# for dotfile in *(/); do
+#   stow -v -t ~/ -S $dotfile
+# done
+
+ansible-playbook -i ./hosts ./dotfiles.yml --ask-become-pass
+
+# If terminal-notifier is installed, use it to display a notification.
+if command -v terminal-notifier 1>/dev/null 2>&1; then
+  terminal-notifier -title "dotfiles: install complete" -message "Successfully set up dev environment."
+fi
