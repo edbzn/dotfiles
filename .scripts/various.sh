2705600475af085f3b42ed4f40f187c0e75a3644
Edouard Bozon <bozonedouard@gmail.com>
2023-03-21 23:17:27 +0100

    Update various.sh
    Add Flameshot

--- .scripts/various.sh
+++ .scripts/various.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install git terminator curl htop vim stow -y
+sudo apt install git terminator curl htop vim stow flameshot -y
 
 # Diff tool for Git
 sudo npm install diff-so-fancy -g
