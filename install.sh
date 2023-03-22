b4872b717ba0462c4fb882f17bea5989fc79ae76
Edouard Bozon <bozonedouard@gmail.com>
2023-03-22 21:30:48 +0100

    refactor: rename

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e 
+set -e
 
 if ! [ -x "$(command -v ansible)" ]; then
   sudo apt install ansible
 fi
 
-ansible-playbook -i ./hosts ./bootstrap.yml --ask-become-pass
+ansible-playbook -i ./hosts ./playbook.yml --ask-become-pass
 
 if command -v notify-send 1>/dev/null 2>&1; then
   notify-send "dotfiles: install complete" "Successfully set up dev environment."
--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@ set -e
 
 if [[ $# -eq 0 ]]; then
   echo "Updating local dev environment..."
-  ansible-playbook -i ./hosts ./bootstrap.yml --skip-tags "bootstrap" --ask-become-pass
+  ansible-playbook -i ./hosts ./playbook.yml --skip-tags "bootstrap" --ask-become-pass
 else
   echo "Updating local dev environment... (--tags $@)"
   # format args for ansible from tagA tagB tagC to "tagA,tagB,tagC"
   tags=$(echo $@ | sed 's/ /,/g')
-  ansible-playbook -i ./hosts ./bootstrap.yml --tags "$tags" --skip-tags "bootstrap" --ask-become-pass
+  ansible-playbook -i ./hosts ./playbook.yml --tags "$tags" --skip-tags "bootstrap" --ask-become-pass
 fi
 
 if command -v notify-send 1>/dev/null 2>&1; then
