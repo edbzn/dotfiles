45eea42a7d317373378eb4509924d9125c683e96
Edouard Bozon <bozonedouard@gmail.com>
2023-03-22 21:27:01 +0100

    refactor: add roles + fix scripts

--- update.sh
+++ update.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
 set -e
 
 if [[ $# -eq 0 ]]; then
-  # Run all roles except for the ones tagged with 'bootstrap'
   echo "Updating local dev environment..."
-  ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/local_env.yml --skip-tags "bootstrap"
+  ansible-playbook -i ./hosts ./bootstrap.yml --skip-tags "bootstrap" --ask-become-pass
 else
   echo "Updating local dev environment... (--tags $@)"
-  ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/local_env.yml --skip-tags "bootstrap" --tags $@
+  # format args for ansible from tagA tagB tagC to "tagA,tagB,tagC"
+  tags=$(echo $@ | sed 's/ /,/g')
+  ansible-playbook -i ./hosts ./bootstrap.yml --tags "$tags" --skip-tags "bootstrap" --ask-become-pass
 fi
 
-# If terminal-notifier is installed, use it to display a notification
-if command -v terminal-notifier 1>/dev/null 2>&1; then
-  terminal-notifier -title "dotfiles: update complete" -message "Successfully updated dev environment."
+if command -v notify-send 1>/dev/null 2>&1; then
+  notify-send "dotfiles: update complete" "Successfully updated dev environment."
 fi
