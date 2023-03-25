73d3ff9d07a2885ce0992821e565eb85880dd0e9
Edouard Bozon <bozonedouard@gmail.com>
2023-03-25 11:41:01 +0100

    fix: requirement.yml

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if ! [ -x "$(command -v ansible)" ]; then
   sudo apt install ansible
 fi
 
+ansible-galaxy install -r ./meta/requirements.yml
 ansible-playbook -i ./hosts ./playbook.yml --ask-become-pass
 
 if command -v notify-send 1>/dev/null 2>&1; then
